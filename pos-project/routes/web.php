9ad283f79ab22eb5d876d1f89fb70934b758a4ff
Ashrafi <ashrafikhanam.18@gmail.com>
2023-07-20 11:11:15 +0600

    filee added

--- pos-project/routes/web.php
+++ pos-project/routes/web.php
@@ -1,18 +1,22 @@
 <?php
 
+use App\Http\Controllers\UserController;
+use App\Http\Middleware\TokenVerificationMiddleware;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
 
 Route::get('/', function () {
     return view('welcome');
 });
+// Start Backend
+Route::post('/user-registration',[UserController::class,'UserRegistration']);
+Route::post('/user-login',[UserController::class,'UserLogin']);
+Route::post('/send-otp',[UserController::class,'SendOTPCode']);
+Route::post('/verify-otp',[UserController::class,'VerifyOTP']);
+// Token Verify
+Route::post('/reset-password',[UserController::class,'ResetPassword'])->middleware([TokenVerificationMiddleware::class]);
+
+// End Backend
+
+// pages
+Route::get('/userLogin',[UserController::class,'LoginPage']);
